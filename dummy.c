00265449cca2c8131f45d6d260e779ea3886d3be
null <Null@horsefucker.org>
2018-03-27 01:03:32 +0100

    Add files via upload

---
+++ dummy.c
@@ -0,0 +1,16 @@
+#include <stdio.h> // standard I/O header file
+
+int main(int argc, char **argv)
+{
+printf("\nKill me when done !!!\n"); // print "Hello World" message on screen
+
+int i;
+for (i = 0; i < 10;)
+{
+
+sleep(30);
+
+}
+
+return(0);
+} 
---
+++ start.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd "/media/Data/Games/LeagueOfLegends/wineprefix/drive_c/League of Legends"
+WINEDEBUG=-all WINEPREFIX="/media/Data/Games/LeagueOfLegends/wineprefix" wine LeagueClient.exe 
+exit
