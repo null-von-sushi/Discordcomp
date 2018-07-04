de1219d6fbb36e517d425a7751b641a720dceaaa
null <Null@horsefucker.org>
2018-07-04 20:46:04 +0100

    deleting wine start script
    deleting old files, cleaning up for the rewrite

--- start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd "/media/Data/Games/LeagueOfLegends/wineprefix/drive_c/League of Legends"
-WINEDEBUG=-all WINEPREFIX="/media/Data/Games/LeagueOfLegends/wineprefix" wine LeagueClient.exe 
-exit
