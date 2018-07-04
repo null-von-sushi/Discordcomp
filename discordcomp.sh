00954e2829981f93158da124dc9be192d71c0006
null <Null@horsefucker.org>
2018-07-04 20:50:25 +0100

    Update main file to include 'thread' capabilities
    Everything is now one file, hopefully nothing breaks welp

--- discordcomp.sh
+++ discordcomp.sh
@@ -19,13 +19,51 @@ mkdir -p /tmp/discordhelper
 rm -rf /tmp/discordhelper/*
 PID=0
 COUNT=1
-while [ true ]
+
+function thread {
+    CURRENT=$1
+    PID=$2
+    DB=$3
+    TMP=$4
+    if [[ -f $TMP/$CURRENT.dummy ]]
+    then
+        echo "$CURRENT already has an instance running or thread was force closed, exiting"
+        exit
+    fi
+    if [[ -z $PID ]]
+    then
+        echo "vars not set, exiting"
+        exit
+    fi
+    cp dummy $TMP/$CURRENT.dummy
+    chmod +x $TMP/$CURRENT.dummy
+    $TMP/$CURRENT.dummy > /dev/null &
+    DUMMYPID=$!
+    GETCURRENTPID=$(pgrep -x "$CURRENT") 
+    while [ true ]
+    do 
+        if [[ "$GETCURRENTPID" == "$PID" ]]
+        then
+            sleep 5
+            GETCURRENTPID=$(pgrep -x "$CURRENT") 
+        else
+            kill -9 $DUMMYPID > /dev/null
+            sleep 1
+            rm -f $TMP/$CURRENT.dummy
+            exit
+        fi
+    done
+    exit
+}
+
+
+while [[ true ]]
 do
     for i in $(seq 1 $(cat $DB | wc -l))
     do
         CURRENT=$(cat $DB | awk "NR == $COUNT")
         PID=$(pgrep -x "$CURRENT")
-        pgrep -x "$CURRENT" > /dev/null 2>&1 #&& echo "$CURRENT is running with PID $PID"
+        pgrep -x "$CURRENT" > /dev/null 2>&1 && echo "$CURRENT is running with PID $PID"
         if [[ $PID == "0" ]]
         then
             true
@@ -33,11 +71,15 @@ do
         then
             true
         else
-            echo "$CURRENT $PID $DB $TMP" > debug.var
-            ./discordcomp.thread.sh "$CURRENT" "$PID" "$DB" "$TMP" > /dev/null & 
+            thread "$CURRENT" "$PID" "$DB" "$TMP" > /dev/null & 
         fi
         COUNT=$((COUNT + 1))
+        if (( $COUNT > $(cat $DB | wc -l) ))
+        then
+            COUNT=1
+        fi
         PID=0
     done
 done
-exit
+
+
